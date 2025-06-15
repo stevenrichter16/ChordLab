281b278efffdb9288bd94226c43dbffbf33c2e0c
stevenrichter16 <stevenrichter16@gmail.com>
2025-06-15 09:53:41 -0500

    Initial Commit

---
+++ ChordLab/ChordLabApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChordLabApp.swift
+//  ChordLab
+//
+//  Created by Steven Richter on 6/15/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChordLabApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ChordLabTests/ChordLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChordLabTests.swift
+//  ChordLabTests
+//
+//  Created by Steven Richter on 6/15/25.
+//
+
+import Testing
+@testable import ChordLab
+
+struct ChordLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
